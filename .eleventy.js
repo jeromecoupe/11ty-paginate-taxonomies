293f17a1a6391f64051aef2755661d9c96fe0d2d
Jérôme Coupé <jerome.coupe@webstoemp.com>
2021-06-18 14:51:52 +0200

    move 11ty files to ./src/_11ty

--- .eleventy.js
+++ .eleventy.js
@@ -2,19 +2,19 @@ module.exports = function (eleventyConfig) {
   // blogposts collection
   eleventyConfig.addCollection(
     "blogposts",
-    require("./eleventy/collections/blogposts.js")
+    require("./src/_11ty/collections/blogposts.js")
   );
 
   // blogposts unique categories
   eleventyConfig.addCollection(
     "blogCategories",
-    require("./eleventy/collections/blogpostsCategories.js")
+    require("./src/_11ty/collections/blogpostsCategories.js")
   );
 
   // blogposts by categories
   eleventyConfig.addCollection(
     "blogpostsByCategories",
-    require("./eleventy/collections/blogpostsByCategories.js")
+    require("./src/_11ty/collections/blogpostsByCategories.js")
   );
 
   return {
